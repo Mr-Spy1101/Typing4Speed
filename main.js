844b2af0648620dad50611ab2f75ec5bf753cdb4
Mr-Spy <mr.spy1101@gmail.com>
2019-07-25 18:26:15 +0300

    cpm and wpm

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-var targetString = "Hello\rWorld !!";
+var targetString = "Hello\rWorld !!\rHello\rWorld !!\rHello\rWorld !!\rHello\rWorld !!\rHello\rWorld !!";
 var records = [];
 var incorrectclicks = 0;
 var index = 0;
 var gameOver = false;
+var timer = 60;
+var playingrecord = false;
+setInterval(() => {
+  DecrementTimer();
+  ViewTimer();
+}, 1000);
 
 function addKeyTyped(e) {
   if (index >= targetString.length || gameOver)
     return;
   if (incorrectclicks >= 4)
     return;
+  if (index == 0)
+    timer = 60;
+
   var keycode = e.keycode || e.which;
   if (keycode == targetString.charCodeAt(index) && incorrectclicks == 0) {
     document.getElementById("targetTypedText").children[index].classList = [];
@@ -32,6 +41,8 @@ function addKeyTyped(e) {
   if (index == targetString.length && incorrectclicks == 0) {
     gameOver = true;
     document.getElementById("viewRecordBtn").disabled = false;
+    var cpm = targetString.length / (60 - timer) * 60;
+    document.getElementById("scoreText").innerHTML = "" + cpm.toFixed(1) + "  CPM" + "<br>" + (cpm / 4).toFixed(1) + "  WPM";
   }
 }
 
@@ -46,6 +57,8 @@ function DeleteKeyTyped(e) {
     return;
   if (incorrectclicks > 0)
     incorrectclicks--;
+  if (index == 1)
+    timer = 60;
   records.push({ KeyCode: keycode, time: Date.now() });
   if (index < targetString.length) {
     document.getElementById("targetTypedText").children[index].classList = [];
@@ -57,16 +70,21 @@ function DeleteKeyTyped(e) {
 }
 
 function ViewRecord() {
+  if (playingrecord)
+    return;
   if (!gameOver)
     return;
   index = 0;
   incorrectclicks = 0;
   initialize();
   ViewRecordAction(0);
+  playingrecord = true;
 }
 
 function ViewRecordAction(moveindex) {
-  if (moveindex == records.length)
+  if (moveindex >= records.length)
+    return;
+  if (!gameOver)
     return;
 
   var keycode = records[moveindex].KeyCode;
@@ -101,8 +119,10 @@ function ViewRecordAction(moveindex) {
     document.getElementById("targetTypedText").children[index].classList = [];
     document.getElementById("targetTypedText").children[index].classList.add("active");
   }
-  if (moveindex + 1 >= records.length)
+  if (moveindex + 1 >= records.length) {
+    playingrecord = false;
     return;
+  }
   setTimeout(function () { ViewRecordAction(moveindex + 1); }, records[moveindex + 1].time - records[moveindex].time);
 }
 
@@ -121,6 +141,20 @@ function PlayAgain() {
   incorrectclicks = 0;
   index = 0;
   gameOver = false;
+  playingrecord = false;
   document.getElementById("viewRecordBtn").disabled = true;
   initialize();
+  timer = 60;
+}
+
+function DecrementTimer() {
+  if (gameOver)
+    return;
+  timer--;
+  if (index == 0)
+    timer = 60;
+}
+
+function ViewTimer() {
+  document.getElementById("timerText").innerHTML = "Timer : " + timer;
 }
