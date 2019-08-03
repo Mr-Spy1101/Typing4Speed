d48a928974ff29dfaf737aabfb64a360937d4709
Mr-Spy <mr.spy1101@gmail.com>
2019-08-03 13:01:01 +0300

    new aaa

--- app.js
+++ app.js
@@ -22,8 +22,17 @@ class player {
     }
 }
 
+class chat {
+    constructor(_playername, _message)
+    {
+        this.data = Date.now();
+        this.playername = _playername;
+        this.message = _message;
+    }
+}
+
 class match {
-    constructor(_textType, _textId, _players, _status, _totaltime, _timer, _lifetime) {
+    constructor(_textType, _textId, _players, _status, _totaltime, _timer, _lifetime, _random) {
         this.textType = _textType;
         this.textId = _textId;
         this.players = _players;
@@ -31,35 +40,39 @@ class match {
         this.totaltime = _totaltime;
         this.timer = _timer;
         this.lifetime = _lifetime;
+        this.chats = [];
+        this.random = _random;
     }
 }
 
 var io = require('socket.io')(serv, {});
 var matches = {};
-var sockets = {};
 
-matches["ppu"] = new match('code', '1', [], 'waiting', 100, 100, 10);
 
 setInterval(() => {
     for (var i in matches) {
-        matches[i].lifetime -= 1 / 25;
+        matches[i].lifetime -= 0.1;
         if (matches[i].lifetime < 0) {
             delete matches[i];
             continue;
         }
         if (matches[i].status == 'running' || matches[i].status == 'gameover')
-            matches[i].timer -= 1 / 25;
+            matches[i].timer -= 0.1;
         if (matches[i].status == 'running' && matches[i].timer <= 0) {
             matches[i].status = 'gameover';
             matches[i].timer = 5;
         }
         if (matches[i].status == 'gameover' && matches[i].timer <= 0) {
-            matches[i].status = 'waiting';
-            matches[i].timer = matches[i].totaltime;
-            matches[i].textid = RandomTextId(matches[i].type);
-            for (var j in matches[i].players)
-                io.sockets.socket(j).emit('RefreshPage');
-            matches[i].players = {};
+            RandomTextId(matches[i].textType, function (result) {
+                matches[i].textId = result;
+
+                matches[i].status = 'waiting';
+                matches[i].timer = matches[i].totaltime;
+                for (var j in matches[i].players) {
+                    matches[i].players[j].ready = false;
+                    io.to(j).emit('RefreshPage');
+                }
+            });
         }
 
         if (matches[i].status == 'waiting') {
@@ -70,165 +83,247 @@ setInterval(() => {
             }
 
             if (readyplayers >= 2) {
+                matches[i].timer = matches[i].totaltime;
                 matches[i].status = 'running';
                 for (var j in matches[i].players)
-                    io.sockets.socket(j).emit('GameStarts');
+                    io.to(j).emit('GameStarts');
             }
         }
     }
-}, 1000 / 25);
+}, 100);
 
 var mysql = require('mysql');
 var con = mysql.createConnection({
-    host: "sql12.freemysqlhosting.net",
-    user: "sql12300455",
-    password: "T82huA8lPE"
+    host: "localhost",
+    user: "root",
+    database: "typing4speed",
+    password: ""
 });
 
-function StringToHTML(textid) {
-    //get text from database
-    var data = "";
-    con.connect(function (err) {
-        if (err) {
-            console.log("DB error");
-            return;
+con.connect(function (err) {
+    if (err)
+        console.log("DB   " + err);
+    else
+        console.log("DB connected");
+});
+
+var fs = require('fs');
+function TargetTextFileToDB(filename, type) {
+    fs.readFile(filename, 'utf8', function (err, data) {
+        var output = "";
+        var lines = data.split('\n');
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i];
+            line = line.replace(/[\t]/, "    ");
+            for (var j = 0; j < line.length; j++)
+                output += line[j];
         }
-        con.query("SELECT text FROM texts WHERE id='" + textid + "';", function (err, result) {
-            data = result[0].text;
-        });
+        output = Buffer.from(data, 'ascii').toString('base64');
+        var sql = "INSERT INTO texts (text, type) VALUES ('" + output + "', '" + type + "');";
+        con.query(sql);
     });
-    //////////////////////////
-    let buff = new Buffer(data, 'base64');
-    data = buff.toString('ascii');
-    //data = window.atob(data);//for base64 decoding
-    var output = "<pre id='targetTypedText'>";
-    var index = 0;
-    var firstchar = true;
-    while (data.indexOf("\r", index) >= 0) {
-        var line = data.substr(index, data.indexOf("\r", index) - index);
-        index = data.indexOf("\r", index) + 2;
-        isNotSpace = false;
-        len = line.length;
-        for (var i = 0; i < len; i++) {
-            if (isNotSpace == false) {
-                if (line[i] != ' ')//if not space
-                {
-                    isNotSpace = true;
-                    if (firstchar) {
-                        output += "<span class='normal-color active'>" + line[i] + "</span>";
-                        firstchar = false;
+}
+//TargetTextFileToDB("code.txt", "code");
+
+function StringToHTML(textid, callback) {
+    var sql = "SELECT text FROM texts WHERE id=" + textid + ";";
+    output = "";
+    con.query(sql, function (err, result) {
+        var data = result[0]["text"];
+        data = Buffer.from(data, 'base64').toString('ascii');
+
+        output = "<pre id='targetTypedText'>";
+        var index = 0;
+        var firstchar = true;
+        while (data.indexOf("\r", index) >= 0) {
+            var line = data.substr(index, data.indexOf("\r", index) - index);
+            index = data.indexOf("\r", index) + 2;
+            isNotSpace = false;
+            len = line.length;
+            for (var i = 0; i < len; i++) {
+                if (isNotSpace == false) {
+                    if (line[i] != ' ')//if not space
+                    {
+                        isNotSpace = true;
+                        if (firstchar) {
+                            output += "<span class='normal-color active'>" + line[i] + "</span>";
+                            firstchar = false;
+                        }
+                        else
+                            output += "<span class='normal-color'>" + line[i] + "</span>";
                     }
+                    else
+                        output += " ";
+                }
+                else {
+                    if (line[i] == ' ')
+                        output += "<span class='normal-color'>" + "<span class='space'>" + line[i] + "</span></span>";
                     else
                         output += "<span class='normal-color'>" + line[i] + "</span>";
                 }
-                else
-                    output += " ";
             }
-            else {
-                if (line[i] == ' ')
-                    output += "<span class='normal-color'>" + "<span class='space'>" + line[i] + "</span></span>";
-                else
-                    output += "<span class='normal-color'>" + line[i] + "</span>";
-            }
-        }
-        if (len > 1)
-            output += "<span class='normal-color enter'><span class='enter'>&#9661;</span></span>";
-        output += "\r";
-    }
-    output += '</pre>';
-    return output;
-}
-
-function StringToString(textid) {
-    var data = "";
-    con.connect(function (err) {
-        if (err) {
-            console.log("DB error");
-            return;
-        }
-        con.query("SELECT text FROM texts WHERE id='" + textid + "';", function (err, result) {
-            data = result[0].text;
-        });
-    });
-
-    let buff = new Buffer(data, 'base64');
-    data = buff.toString('ascii');
-    console.log(data);
-    //data = window.atob(data);//for base64 decoding
-
-    var output = "";
-    var index = 0;
-    while (data.indexOf("\r", index) >= 0) {
-        var line = data.substr(index, data.indexOf("\r", index) - index);
-        index = strpos(data, "\r", index) + 2;
-        var isNotSpace = false;
-        var len = line.length;
-        for (var i = 0; i < len; i++) {
-            if (isNotSpace == false) {
-                if (line[i] != ' ')//if not space
-                {
-                    isNotSpace = true;
-                    output += $line[$i];
-                }
-            }
-            else
-                output += $line[$i];
-        }
-        if (len > 1)
+            if (len > 1)
+                output += "<span class='normal-color enter'><span class='enter'>&#9661;</span></span>";
             output += "\r";
-    }
-    return output;
+        }
+        output += '</pre>';
+        return callback(output);
+    });
 }
 
-function RandomTextId(type) {
-    var data;
-    con.connect(function (err) {
-        con.query("SELECT id FROM texts WHERE type='" + type + "' ORDER BY RAND();", function (err, result) {
-            if (err) throw err;
-            data = result[0].id;
-        });
+function StringToString(textid, callback) {
+    var sql = "SELECT text FROM texts WHERE id=" + textid + ";";
+    con.query(sql, function (err, result) {
+        var data = result[0]["text"];
+        data = Buffer.from(data, 'base64').toString('ascii');
+
+        var output = "";
+        var index = 0;
+        while (data.indexOf("\r", index) >= 0) {
+            var line = data.substr(index, data.indexOf("\r", index) - index);
+            index = data.indexOf("\r", index) + 2;
+            var isNotSpace = false;
+            var len = line.length;
+            for (var i = 0; i < len; i++) {
+                if (isNotSpace == false) {
+                    if (line[i] != ' ')//if not space
+                    {
+                        isNotSpace = true;
+                        output += line[i];
+                    }
+                }
+                else
+                    output += line[i];
+            }
+            if (len > 1)
+                output += "\r";
+        }
+        return callback(output);
     });
-    return data;
+}
+
+function RandomTextId(type, callback) {
+    con.query("SELECT id FROM texts WHERE type='" + type + "' ORDER BY RAND();", function (err, result) {
+        if(result.length>0)
+            return callback(result[0]["id"]);
+        else
+            return callback(-1);
+    });
+}
+
+function CreateNewMatch(type, israndom, callback)
+{
+    RandomTextId(type, function(textid){
+        matchid = Date.now();
+        matches[matchid] = new match(type, textid, {}, "waiting", 60, 60, 20, israndom);
+        return callback(matchid);
+    });
 }
 
 io.sockets.on('connection', function (socket) {
     console.log('socket connection ' + socket.id);
-    sockets[socket.id] = socket;
+    socket.emit('ResponsePlayerId', { playerid: socket.id });
 
     socket.on('RequestJoinPlayerToMatch', function (data) {
-        newplayer = new player(data.playername, false, 0, 0, 0, 0);
-        console.log(Object.keys(matches).length);
-        for (var x in matches)
-            console.log(x);
-        matches[data.matchid].players[socket.id] = newplayer;
-        socket.emit('ResponseJoinPlayerToMatch');
+        var joinstatus = data.matchid in matches;
+        var matchstatus = "waiting";
+        if (joinstatus == true) {
+            newplayer = new player(data.playername, false, 0, 0, 0, 0);
+            matches[data.matchid].players[socket.id] = newplayer;
+            matchstatus = matches[data.matchid].status;
+        }
+        socket.emit('ResponseJoinPlayerToMatch', {
+            matchstatus: matchstatus,
+            joinstatus: joinstatus
+        });
+    });
+
+    socket.on('PracticeRequestData', function(data){
+        if(data.type!="plain" && data.type!="cpp" && data.type!="python" && data.type!="java")
+            return;
+
+        RandomTextId(data.type, function(textid){
+            if(textid==-1)
+                return;
+
+            StringToHTML(textid, function(htmltext){
+                socket.emit('ResponsePracticeHTML', {
+                    text: htmltext
+                });
+            });
+            StringToString(textid, function(stringtext){
+                socket.emit('ResponsePracticeString', {
+                    text: stringtext
+                });
+            });
+        });
+    });
+
+    socket.on('RequestJoinToRandomMatch', function(data){
+        var matchid = -1;
+        for(i in matches)
+        {
+            if(matches[i].textType == data.type && matches[i].random==true && Object.keys(matches[i].players).length < 4)
+                matchid = i;
+        }
+
+        if(matchid!=-1)
+        {
+            socket.emit('ResponseJoinToRandomMatch', {
+                matchid: matchid
+            });
+            return;
+        }
+        
+        CreateNewMatch(data.type, true, function(result){
+            socket.emit('ResponseJoinToRandomMatch', {
+                matchid: result
+            });
+        });
+    });
+
+    socket.on('RequestCreateNonRandomMatch', function(data){
+        CreateNewMatch(data.type, false, function(matchid){
+            socket.emit('ResponseCreateNonRandomMatch', {
+                matchid: matchid
+            });
+        });
     });
 
     socket.on('RequestTextHTML', function (data) {
-        socket.emit('ResponseTextHTML', {
-            html: StringToHTML(matches[data.matchid].textId)
+        StringToHTML(matches[data.matchid].textId, function (result) {
+            socket.emit('ResponseTextHTML', {
+                html: result
+            });
         });
     });
 
     socket.on('RequestTextString', function (data) {
-        socket.emit('ResponseTextHTML', {
-            text: StringToString(matches[data.matchid].textid)
+        StringToString(matches[data.matchid].textId, function (result) {
+            socket.emit('ResponseTextString', {
+                text: result
+            });
         });
     });
 
     socket.on('SendPlayerData', function (data) {
         matches[data.matchid].lifetime = 10;
-        console.log(matches[data.matchid].players[socket.id].wpm);
-        matches[data.matchid].players[socket.id].characterreaches = data.characterreaches;
-        matches[data.matchid].players[socket.id].recordscount = data.recordscount;
-        if (matches[data.matchid].totaltime - matches[data.matchdata].timer > 0)
-            matches[data.matchid].players[socket.id].wpm = index * 60 / 4 / (matches[data.matchid].totaltime - matches[data.matchdata].timer);
-        else
-            matches[data.matchid].players[socket.id].wpm = 0;
-        if (data.recordscount > 0)
-            matches[data.matchid].players[socket.id].accuracy = data.characterreaches / data.recordscount * 100;
-        else
-            matches[data.matchid].players[socket.id].accuracy = 0;
+        if (matches[data.matchid].status == "running") {
+            matches[data.matchid].players[socket.id].characterreaches = data.characterreaches;
+            matches[data.matchid].players[socket.id].recordscount = data.recordscount;
+            if (matches[data.matchid].totaltime - matches[data.matchid].timer > 0)
+                matches[data.matchid].players[socket.id].wpm = data.characterreaches * 60 / 5 / (matches[data.matchid].totaltime - matches[data.matchid].timer);
+            else
+                matches[data.matchid].players[socket.id].wpm = 0;
+            if (data.recordscount > 0)
+                matches[data.matchid].players[socket.id].accuracy = data.characterreaches / data.recordscount * 100;
+            else
+                matches[data.matchid].players[socket.id].accuracy = 0;
+        }
+    });
+
+    socket.on('ResuestAddChatToMatch', function(data){
+        matches[data.matchid].chats.push(new chat(data.playername, data.message));
     });
 
     socket.on('RequestMatchData', function (data) {
@@ -237,19 +332,14 @@ io.sockets.on('connection', function (socket) {
         });
     });
 
-    socket.on('GetPlayerReady', function (data) {
+    socket.on('SetPlayerReady', function (data) {
         matches[data.matchid].players[socket.id].ready = true;
     });
 
-    socket.on('disconnect', function (socket) {
-        delete sockets[socket.id];
+    socket.on('disconnect', function () {
+        console.log("player deleted");
         for (i in matches)
             delete matches[i].players[socket.id];
     });
 
-    //io.sockets.socket(id).emit('hello');
-
-
 });
-
-
