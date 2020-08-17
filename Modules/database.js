4651bd7947fb759da48dbe7f4fddc6f3d93726d3
Mr-Spy1101 <53275264+Mr-Spy1101@users.noreply.github.com>
2020-08-17 15:08:03 +0300

    Update database.js

--- Modules/database.js
+++ Modules/database.js
@@ -94,15 +94,37 @@ let java =
 
 let python = 
 [
-`def jpeg_res(filename):
-with open(filename,'rb') as img_file:
-   img_file.seek(163)
-   a = img_file.read(2)
-   height = (a[0] << 8) + a[1]
-   a = img_file.read(2)
-   width = (a[0] << 8) + a[1]
-print("The resolution of the image is",width,"x",height)
-jpeg_res("img1.jpg")
+`def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    return x / y
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    choice = input("Enter choice(1/2/3/4): ")
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        break
+    else:
+        print("Invalid Input")
 `
 ];
 
